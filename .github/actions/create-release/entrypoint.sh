f083b5a9bb13c9f38ef0fb464d54730da077af47
paulssonjoel <105986177+paulssonjoel@users.noreply.github.com>
2026-08-09 17:50:07 +0200

    Fix create-release

--- .github/actions/create-release/entrypoint.sh
+++ .github/actions/create-release/entrypoint.sh
@@ -2,13 +2,21 @@
 
 set -e
 cd /github/workspace
+
+# Mounting messes up permissions
 git config --global --add safe.directory /github/workspace
-git -C /github/workspace rev-parse --show-toplevel
-ls -A
+
+# Commit as webredax
 git config user.name webredax
 git config user.email 309066399+webredax@users.noreply.github.com
+
+# Add any changes made by the job so far
 git add .
+
+# Commit regardless of if there were any changes or not
 git commit --allow-empty -m "Bump version to $INPUT_NEW_VERSION"
+
+# Tag and push
 git tag "v$INPUT_NEW_VERSION"
 git push origin main
 git push origin "v$INPUT_NEW_VERSION"
