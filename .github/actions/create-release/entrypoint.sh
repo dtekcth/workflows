49637020f4d7f23a0f4f2c41b50aac6cfa2cfc73
paulssonjoel <105986177+paulssonjoel@users.noreply.github.com>
2026-08-18 17:32:19 +0200

    create-release: Do not create commit

--- .github/actions/create-release/entrypoint.sh
+++ .github/actions/create-release/entrypoint.sh
@@ -30,19 +30,8 @@ fi
 # Save version for output
 echo "new_version=$INPUT_NEW_VERSION" >>"$GITHUB_OUTPUT"
 
-# Commit as webredax
-git config user.name webredax
-git config user.email 309066399+webredax@users.noreply.github.com
-
-# Add any changes made by the job so far
-git add .
-
-# Commit regardless of if there were any changes or not
-git commit --allow-empty -m "Bump version to $INPUT_NEW_VERSION"
-
-# Tag and push
+# Tag latest commit
 git tag "v$INPUT_NEW_VERSION"
-git push origin main
 git push origin "v$INPUT_NEW_VERSION"
 
 # Create GitHub release
